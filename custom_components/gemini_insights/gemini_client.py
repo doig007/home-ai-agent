f061898d31d922cdeb6b8560d10e922da87c4d95
doig007 <doig@alum.mit.edu>
2025-07-21 22:26:01 +0100

    Alternative fix of template formatting

--- custom_components/gemini_insights/gemini_client.py
+++ custom_components/gemini_insights/gemini_client.py
@@ -102,8 +102,7 @@ class GeminiClient:
 
         entity_data = json.loads(entity_data_json or '{}')
 
-        from string import Template
-        formatted_prompt = Template(prompt).substitute(
+        formatted_prompt = prompt.format(
             long_term_stats=json.dumps(entity_data.get("long_term_stats", {}), indent=2),
             recent_events=json.dumps(entity_data.get("recent_events", {}), indent=2),
         )
